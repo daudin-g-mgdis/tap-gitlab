893e621f7aa5231eae8264f434369c6b84adaa33
Yannis Roussos <iroussos@gitlab.com>
2019-09-30 20:33:53 +0300

    Add the project's name to gitlab_stats_merged_mrs_per_month

--- models/transform/gitlab_stats_merged_mrs_per_month.sql
+++ models/transform/gitlab_stats_merged_mrs_per_month.sql
@@ -1,4 +1,12 @@
-WITH merged_merge_requests AS (
+WITH projects AS (
+
+	SELECT project_id, project_name
+	
+	FROM {{ref('gitlab_projects')}}
+
+),
+
+merged_merge_requests AS (
 
 	SELECT 
 		project_id, 
@@ -17,16 +25,23 @@ WITH merged_merge_requests AS (
 toral_mrs_per_month AS (
 
 	SELECT 
-		project_id, 
-		merged_year, 
-		merged_month, 
-		count(distinct merge_request_id) as total_mrs, 
-		count(distinct author_id) as total_authors, 
-		round(avg(days_to_merge), 2) as days_to_merge
+		projects.project_id as project_id,
+		projects.project_name as project_name, 
+		mrs.merged_year as merged_year, 
+		mrs.merged_month as merged_month, 
+		count(distinct mrs.merge_request_id) as total_mrs, 
+		count(distinct mrs.author_id) as total_authors, 
+		round(avg(mrs.days_to_merge), 2) as days_to_merge
 
-	FROM merged_merge_requests
+	FROM merged_merge_requests mrs
+	  LEFT JOIN projects   
+	    ON mrs.project_id = projects.project_id
 
-	GROUP BY project_id, merged_year, merged_month
+	GROUP BY 
+	  projects.project_id, 
+	  projects.project_name, 
+	  mrs.merged_year, 
+	  mrs.merged_month
 
 )
 
